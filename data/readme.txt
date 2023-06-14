73f03704142fb1a3efcf38bdf9161e7ccf4c4d81
Jackques <Gommeren@msn.com>
2023-06-14 21:10:33 +0200

    Updated code by following tutorial; vid 17

---
+++ data/readme.txt
@@ -0,0 +1,5 @@
+// JSON Server automatically creates the following endpoints for you:, based on the content of the key-value inside the first object-literal in db.json;
+/blogs      GET     Fetch all blogs
+/blogs/{id} GET     Fetch a single blog
+/blogs      POST    Add a new blog
+/blogs/{id} DELETE  Delete a blog
--- src/Home.js
+++ src/Home.js
@@ -2,16 +2,20 @@ import {useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {title: "first title", body: "Lorum ipsum...", author: "Jack", id: 1},
-        {title: "second title", body: "Lorum ipsum...", author: "Tim", id: 2},
-        {title: "third title", body: "Lorum ipsum...", author: "Jack", id: 3},
-    ]);
+    const [blogs, setBlogs] = useState(
+        //[
+        // {title: "first title", body: "Lorum ipsum...", author: "Jack", id: 1},
+        // {title: "second title", body: "Lorum ipsum...", author: "Tim", id: 2},
+        // {title: "third title", body: "Lorum ipsum...", author: "Jack", id: 3},
+        // above blogs are dummy data to test if it works correctly
+    //]
+        null // since this is the defaultvalue, we cannot 'map/loop' over this value when setting the blogs initially, thus we should first check if blogs is available
+    );
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter((blog) => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    // const handleDelete = (id) => {
+    //     const newBlogs = blogs.filter((blog) => blog.id !== id);
+    //     setBlogs(newBlogs);
+    // }
 
     useEffect(() => {
         console.log('this fires on every render (i.e. init), so if i remove a blog.. i get executed again! (re-render)');
@@ -23,16 +27,41 @@ const Home = () => {
         // which would cause this 'useEffect' to be run again, and so forth
     });
 
+    useEffect(() => {
+        console.log('this fires on every init & manipulation of the attached dependancies');
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                console.log('Data from JSON Server: ', data);
+                // Asynchronously set the blogs
+                setBlogs(data);
+            })
+    }, []);
+
+    const [name, setName] = useState('Mario'); // this is a dependency of the effect below
+    useEffect(() => {
+        console.log('this fires on every init & manipulation of the attached dependancies');
+        console.log('name is: ', name);
+    }, [name]);
+
     return (
         <div className="home">
             <h1>Homepage</h1>
 
             <div className="blog-preview">
-                <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}></BlogList>
-                <BlogList blogs={blogs.filter((blog) => blog.author === "Jack")} title="Jack's Blogs"
-                          handleDelete={handleDelete}></BlogList>
+                {/*{blogs && <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}></BlogList>}*/}
+                {/*{blogs && <BlogList blogs={blogs.filter((blog) => blog.author === "Jack")} title="Jack's Blogs"*/}
+                {/*          handleDelete={handleDelete} />}*/}
+
+                {blogs && <BlogList blogs={blogs} title="All blogs"></BlogList>}
+                {blogs && <BlogList blogs={blogs.filter((blog) => blog.author === "Jack")} title="Jack's Blogs"/>}
+
             </div>
+            <button onClick={()=>{setName('Luigi')}}> I change the state only for 'name' effect</button>
         </div>);
 }
+// Everything between { } is JS thus we can write (conditional) logic here i.e. checking if blogs is not null when we pass it on to BlogList
 
 export default Home;
